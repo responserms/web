28fc60469a51acbfeb7c506e600ab2ecd735fd7c
Spencer Young <spjy@hawaii.edu>
2021-02-07 18:26:05 -1000

    create dropdown component

---
+++ packages/app/components/Dropdown/Dropdown.vue
@@ -0,0 +1,48 @@
+<template>
+  <CSelect :placeholder="placeholder" :size="size" :icon="icon" :iconSize="iconSize" :variant="variant" :isDisabled="isDisabled" :isInvalid="isInvalid" :isRequired="isRequired" :isReadOnly="isReadOnly">
+    <slot />
+  </CSelect>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@nuxtjs/composition-api'
+
+type Variant = 'outline' | 'unstyled' | 'flushed' | 'filled'
+type Size = 'sm' | 'md' | 'lg'
+
+export default defineComponent({
+  props: {
+    placeholder: {
+      type: String
+    },
+    size: {
+      type: Object as PropType<Size>,
+    },
+    icon: {
+      type: String
+    },
+    iconSize: {
+      type: Number
+    },
+    variant: {
+      type: Object as PropType<Variant>
+    },
+    isDisabled: {
+      type: Boolean
+    },
+    isInvalid: {
+      type: Boolean
+    },
+    isRequired: {
+      type: Boolean
+    },
+    isReadOnly: {
+      type: Boolean
+    },
+    rootProps: {
+      type: Boolean
+    },
+  },
+  setup() {},
+})
+</script>
